7e28d833b969efc5f46113b8a6da9733473a4eb4
Kai <kaigustke@gmail.com>
2020-07-24 07:50:21 -0700

    Removed logo import from app.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
 import Header from '../Header'
 import Body from '../Body'
 import Footer from '../Footer'
@@ -13,22 +12,5 @@ function App() {
     </div>
   );
 }
-
-//    <div className="App">
-//      <header className="App-header">
-//        <img src={logo} className="App-logo" alt="logo" />
-//        <p>
-//          Edit <code>src/App.js</code> and save to reload.
-//        </p>
-//        <a
-//          className="App-link"
-//          href="https://reactjs.org"
-//          target="_blank"
-//          rel="noopener noreferrer"
-//        >
-//          Learn React
-//        </a>
-//      </header>
-//    </div>
 
 export default App;
