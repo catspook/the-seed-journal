050bd5f3cfd12e3198e7d5efeb68f94e211d8ce6
Kai <kaigustke@gmail.com>
2020-07-31 18:16:25 -0700

    Chart tab basic stuff

---
+++ src/components/Chart/ChartBase.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Chart from "chart.js"
+
+class ChartBase extends React.Component{
+
+
+    render(){
+        return(
+            let ctx = document.getElementById('myChart')
+            <canvas id="myChart"></canvas>
+        )
+    }
+}
+
+export default ChartBase
