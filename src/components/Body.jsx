e0f50d5fa10f3689b9ee6716b10e10630d6a1044
Kai <kaigustke@gmail.com>
2020-08-05 14:39:50 -0700

    Completed ph scale element

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-//import logo from '../images/logo.svg'; /*Place Holder image*/
-//import Container from 'react-bootstrap/Container'
-//import Row from 'react-bootstrap/Row'
-//import Col from 'react-bootstrap/Col'
 import SearchBase from './Search/SearchBase'
-import PhBar from './Chart/pHBar'
 import '../styles/scss/Body.scss'
 
 class Body extends React.Component {
@@ -13,72 +8,17 @@ class Body extends React.Component {
         return(
             <div>
                 <SearchBase />
-                <PhBar />
             </div>
         )
-    }
-
-    // Content to render on the "insert name here" screen
-    renderTab1() {
-        return(
-            <div>
-            </div>
-        )
-    }
-
-    // Content to render on the "insert name here" screen
-    renderTab2() {
-        return(
-            <div>
-                <p>I'm a Plant!</p>
-            </div>
-        )
-    }
-
-    // Conditionally render the contents of the body depending on
-    // the pageId (Header Buttons)
-    renderBody() {
-        if(this.props.pageId === 0)
-            return this.renderHome()
-        if(this.props.pageId === 1)
-            return this.renderTab1()
-        if(this.props.pageId === 2)
-            return this.renderTab2()
     }
 
     render() {
         return (
             <div className='body'>
-                {this.renderBody()}
+                {this.renderHome()}
             </div>
         )
     }
 }
-
-
-
-//class Info extends React.Component {
-//    render() {
-//        return(
-//           <Container>
-//                <Row>
-//                    <Col>
-//                        <img src={logo} alt='place holder'></img>
-//                    </Col>
-//                    <Col>
-//                        <div className='info'>
-//                            {/*Chang displayed info as needed*/}
-//                            <p>Common Name:</p>
-//                            <p>Scientific Name:</p>
-//                            <p>Growth Zone:</p>
-//                            <p>pH:</p>
-//                        </div>
-//                        <button className='fav-btn'>Add to favorites</button>
-//                    </Col>
-//                </Row>
-//            </Container> 
-//        )
-//    }
-//}
 
 export default Body
--- src/components/Chart/pHBar.jsx
+++ src/components/Chart/pHBar.jsx
@@ -1,48 +1,108 @@
 import React from "react";
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import '../../styles/scss/pHBar.scss'
 
 class PhBar extends React.Component{
     constructor(props){
         super(props)
         this.ph = {
-            0: ["rgba(178, 36, 41, 1)", "rgba(178, 36, 41, 0.2)"],
-            1: ["rgba(231, 108, 13, 1)", "rgba(231, 108, 13, 0.2)"],
-            2: ["rgba(241, 145, 19, 1)", "rgba(241, 145, 19, 0.2)"],
-            3: ["rgba(237, 225, 53, 1)", "rgba(237, 225, 53, 0.2)"],
-            4: ["rgba(197, 235, 45, 1)", "rgba(197, 235, 45, 0.2)"],
-            5: ["rgba(120, 227, 38, 1)", "rgba(120, 227, 38, 0.2)"],
-            6: ["rgba(58, 209, 21, 1)", "rgba(58, 209, 21, 0.2)"],
-            7: ["rgba(36, 172, 21, 1)", "rgba(36, 172, 21, 0.2)"],
-            8: ["rgba(22, 177, 92, 1)", "rgba(22, 177, 92, 0.2)"],
-            9: ["rgba(20, 200, 143, 1)", "rgba(20, 200, 143, 0.2)"],
-            10: ["rgba(21, 176, 203, 1)", "rgba(21, 176, 203, 0.2)"],
-            11: ["rgba(26, 91, 183, 1)", "rgba(26, 91, 183, 0.2)"],
-            12: ["rgba(88, 76, 200, 1)", "rgba(88, 76, 200, 0.2)"],
-            13: ["rgba(126, 65, 180, 1)", "rgba(126, 65, 180, 0.2)"],
-            14: ["rgba(98, 49, 135, 1)", "rgba(98, 49, 135, 0.2)"],
+            0: "rgba(178, 36, 41, 1)", 
+            1: "rgba(231, 108, 13, 1)",
+            2: "rgba(241, 145, 19, 1)",
+            3: "rgba(237, 225, 53, 1)",
+            4: "rgba(197, 235, 45, 1)",
+            5: "rgba(120, 227, 38, 1)",
+            6: "rgba(58, 209, 21, 1)", 
+            7: "rgba(36, 172, 21, 1)", 
+            8: "rgba(22, 177, 92, 1)", 
+            9: "rgba(20, 200, 143, 1)",
+            10: "rgba(21, 176, 203, 1)",
+            11: "rgba(26, 91, 183, 1)",
+            12: "rgba(88, 76, 200, 1)",
+            13: "rgba(126, 65, 180, 1)",
         }
+        this.state = {
+            min: this.props.min,
+            max: this.props.max,
+            valid: true,
+        }
+    }
+    
+    componentWillMount(){
+        if(typeof(this.state.min) !== "number" || typeof(this.state.max) !== "number")
+            this.setState({
+                valid: false,
+            })
+    }
+
+    setUniqueKey(index, value){
+        return index * 10 + value
+    }
+
+    setOpacity(index, value){
+        const num = this.setUniqueKey(index, value)
+        const min = this.state.min * 10
+        const max = this.state.max * 10 - 1
+        if(num >= min && num <= max)
+            return 1
+        return 0.2
+    }
+
+    renderInvalid(){
+        return(
+            <span className="error">No pH Information Available</span>
+        )
+    }
+
+    renderScale(){
+        const repeat = [0,1,2,3,4,5,6,7,8,9]
+        const phVal =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+        return(
+                <div className="container">
+                    <div className="ph-scale">
+                        {
+                            Object.keys(this.ph).map((key,index) =>
+                                repeat.map((value) => 
+                                    <div
+                                        className="ph-node"
+                                        key={this.setUniqueKey(index, value)}
+                                        style={{
+                                            backgroundColor: `${this.ph[key]}`,
+                                            opacity: this.setOpacity(index, value),
+                                        }}
+                                    >
+                                        {this.setUniqueKey(index, value) !== 0 && 
+                                            this.setUniqueKey(index, value) % 2 === 0 &&
+                                            <div className="tick"></div>}
+                                        {this.setUniqueKey(index, value) === this.state.min * 10 &&
+                                            <div className="min-container">
+                                                <span className="min">min: {this.state.min}</span>
+                                            </div>}
+                                        {this.setUniqueKey(index, value) === this.state.max * 10 - 1 &&
+                                            <div className="max-container">
+                                                <span className="max">max: {this.state.max}</span>
+                                            </div>}
+                                    </div>
+                                )
+                            )
+                        }
+                    </div>
+                    <div className="ph-label">
+                        {
+                            phVal.map((key) =>
+                                <span key={key} className="label">
+                                    {key < 10 ? '0' + key : key}
+                                </span>
+                            )
+                        }
+                    </div>
+                    {!this.state.valid && this.renderInvalid()}
+                </div>
+        )
     }
 
     render(){
         return(
-            <Container>
-                <Row className="ph-scale">
-                    {
-                        Object.keys(this.ph).map((key, index) => 
-                            <Col
-                                className="ph-node"
-                                key={index}
-                                id={index}
-                                style={{backgroundColor: `${this.ph[key][0]}`}}
-                            >
-                            </Col>
-                        )
-                    }
-                </Row>
-            </Container>
+            this.renderScale()
         )
     }
 }
